f86101ce73cef7d4d6831020f916c2bfe785d19f
Olli Wilkman <olli.wilkman@iki.fi>
2021-12-15 23:56:09 +0200

    Add type hints to code

--- ruuvi_influx.py
+++ ruuvi_influx.py
@@ -3,14 +3,15 @@ Listen to RuuviTags and upload to influxdb.
 """
 
 import toml
-from influxdb import InfluxDBClient
-from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from sys import exit, argv
 import logging
+
+from influxdb import InfluxDBClient
+from pydantic import BaseModel, Field
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from typing import Dict, List, Optional, Any, MutableMapping
+from sys import exit, argv
 from pprint import pformat
 
-from typing import Dict, List, Optional, Any, MutableMapping
-from pydantic import BaseModel, Field
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -19,7 +20,7 @@ LOG_LEVEL = {
     "DEBUG": logging.DEBUG,
     "INFO": logging.INFO,
     "WARNING": logging.WARNING,
-    "ERROR": logging.ERROR
+    "ERROR": logging.ERROR,
 }
 
 
@@ -39,52 +40,51 @@ class Config(BaseModel):
 
 
 def read_config(filename: str) -> Config:
-    """ Parse Config object from TOML file.
-    """
-    with open(filename, 'r') as f:
+    """Parse Config object from TOML file."""
+    with open(filename, "r") as f:
         return Config.parse_obj(toml.loads(f.read()))
 
 
 def connect_influxdb(config: Config) -> InfluxDBClient:
-    """ Return an InfluxDB client object from given config.
-    """
-    client = InfluxDBClient(host=config.host,
-                            port=config.port,
-                            username=config.username,
-                            password=config.password,
-                            ssl=config.ssl,
-                            verify_ssl=config.ssl)
+    """Return an InfluxDB client object from given config."""
+    client = InfluxDBClient(
+        host=config.host,
+        port=config.port,
+        username=config.username,
+        password=config.password,
+        ssl=config.ssl,
+        verify_ssl=config.ssl,
+    )
     client.switch_database(config.database)
     return client
 
 
 def map_mac(config: Config, mac: str) -> Optional[str]:
-    """Maybe get a device name based on MAC address.
-    """
-    return (None if config.device_names is None
-            else config.device_names.get(mac, None))
+    """Maybe get a device name based on MAC address."""
+    return None if config.device_names is None else config.device_names.get(mac, None)
 
 
 def get_location(config: Config, name: Optional[str]) -> Optional[str]:
-    """Maybe get a named location of a tag from the config.
-    """
-    return (None if (config.locations is None or name is None)
-            else config.locations.get(name, None))
+    """Maybe get a named location of a tag from the config."""
+    return (
+        None
+        if (config.locations is None or name is None)
+        else config.locations.get(name, None)
+    )
 
 
 def ruuvi_to_point(config: Config, received_data: List) -> Dict[str, Any]:
-    """ Format measurement JSON from RuuviTag into InfluxDB data point JSON-like.
-    """
+    """Format measurement JSON from RuuviTag into InfluxDB data point JSON-like."""
     mac: str = received_data[0]
     payload: MutableMapping[str, Any] = received_data[1]
     data_format = payload.get("data_format", None)
     device_name = map_mac(config, mac)
 
     tags = {
-        'mac': mac,
-        'format': data_format,
-        'device': device_name,
-        'location': get_location(config, device_name)
+        "mac": mac,
+        "format": data_format,
+        "device": device_name,
+        "location": get_location(config, device_name),
     }
 
     fields = {}
@@ -92,18 +92,11 @@ def ruuvi_to_point(config: Config, received_data: List) -> Dict[str, Any]:
         if field_name in payload:
             fields[field_name] = payload[field_name]
 
-    return {
-        'measurement': config.measurement,
-        'tags': tags,
-        'fields': fields
-    }
+    return {"measurement": config.measurement, "tags": tags, "fields": fields}
 
 
-def ruuvi_callback(config: Config,
-                   client: InfluxDBClient,
-                   received_data: List) -> None:
-    """ Callback for ruuvi get_datas(). Format the JSON and send to Influx.
-    """
+def ruuvi_callback(config: Config, client: InfluxDBClient, received_data: List) -> None:
+    """Callback for ruuvi get_datas(). Format the JSON and send to Influx."""
     json_body = ruuvi_to_point(config, received_data)
     logger.info(f"Trying to upload: {pformat(json_body)}")
     client.write_points([json_body])
