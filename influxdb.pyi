1aafea4d9a7a35c0c46884be297874b6aa7fa709
Olli Wilkman <olli.wilkman@iki.fi>
2021-12-15 23:56:02 +0200

    Add type hints for influxdb

--- influxdb.pyi
+++ influxdb.pyi
@@ -1,14 +1,14 @@
 from typing import Optional, MutableMapping, Any, List
 
 class InfluxDBClient:
-    def __init__(self,
-                 host: Optional[str],
-                 port: Optional[int],
-                 username: Optional[str],
-                 password: Optional[str],
-                 ssl: Optional[bool],
-                 verify_ssl: Optional[bool]): ...
-
+    def __init__(
+        self,
+        host: Optional[str],
+        port: Optional[int],
+        username: Optional[str],
+        password: Optional[str],
+        ssl: Optional[bool],
+        verify_ssl: Optional[bool],
+    ): ...
     def switch_database(self, db: str) -> None: ...
-
     def write_points(self, points: List[MutableMapping[str, Any]]) -> None: ...
